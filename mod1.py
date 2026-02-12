2e715d904ef0e8f4db83f88470a21125d0a027b3
Nate Minty <nateminty@gmail.com>
2026-02-11 23:03:48 -0500

    switched data sets and got basic line plot working

--- mod1.py
+++ mod1.py
@@ -1,11 +1,38 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
+import numpy as np
 
-df = pd.read_csv('bestsellinbooks2025.csv')
+#df = pd.read_csv('bestsellinbooks2025.csv')
+
+df2 = pd.read_csv('price_of_healthy_diet_clean.csv')
+
+
+us_rows = df2[df2["country"] == "United States of America"]
+
+#print(us_rows.iloc[6])
+
+#print(us_rows.annual_cost_healthy_diet_usd)
+
+#us_rows.annual_cost_healthy_diet_usd.plot(kind='hist', bins=10, edgecolor='black')
+
+#plt.show()
+
+x = us_rows.year
+y = us_rows.annual_cost_healthy_diet_usd
+
+plt.plot(x, y)
+plt.xlabel("Year")
+plt.ylabel("Anual Cost (USD)")
+plt.title("Annual Cost of Healthy Diet in the USA")
+plt.show()
+
+
+
+
 
 #print(df.head())
 
-top_genre = (df.Genre.value_counts())
+#top_genre = (df.Genre.value_counts())
 
 #print(top_genre)
 
